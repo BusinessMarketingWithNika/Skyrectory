f325c4808e8bc99a62de8df216d4f1eafb613938
Nika <businessmarketingwithnika@gmail.com>
2025-01-07 23:52:49 +0100

    JS – categories

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ document.addEventListener("DOMContentLoaded", ()) => {
         { id: 4, name: "Tool 4", description: "Boost your efficiency", image: "/placeholder.svg?height=150&width=150", type: "paid", categories ["Schedulers", "Directories"], date: "2025-06-01" },
         // Add more tools as needed
     ];
+
+    const categories = [ "Analytics", "Schedulers", "Directories", "Chrome Extensions"];
 }
