9bda6fb1958a53bfccbc18608cc15e3bd42b28fe
Nika <businessmarketingwithnika@gmail.com>
2025-02-08 23:54:34 +0100

    export function ToolBadge

--- components/tool-badge.tsx
+++ components/tool-badge.tsx
@@ -4,3 +4,10 @@ interface ToolBadgeProps {
     type: 'featured'| 'free' | 'paid' 
     className?: string
 }
+
+export function ToolBadge({ type, className = '' } : ToolBadgeProps) {
+    switch (type) {
+        case 'featured':
+          return
+    }
+}
