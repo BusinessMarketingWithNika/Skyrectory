510f4dabb50a7cc6c79ac8d2a982c8cfe2e95fb9
Nika <businessmarketingwithnika@gmail.com>
2025-02-20 00:31:01 +0100

    importing stuff

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { FaTwitter, FaFacebook, FaInstagram, FaGithub } from "react-icons/fa"
+
+export function Footer() {
+    const [email, setEmail] = useState ("")
+}
