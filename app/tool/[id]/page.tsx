a895de9648fd1ef18fe892de3cc11c894f903d56
Nika <businessmarketingwithnika@gmail.com>
2025-02-26 00:46:23 +0100

    imports

---
+++ app/tool/[id]/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import { useParams } from "next/navigation"
+import { FloatingHeader } from "@/components/FloatingHeader"
+import { Footer } from "@/components/Footer"
+import { ToolBadge } from "@/components/ui/tool-badge"
+import Link from "next/link"
